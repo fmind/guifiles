751bf775ec1b0bd7ad4e9e563fd9e83bb42e53f8
Médéric Hurier <dev@fmind.me>
2018-12-07 21:51:51 +0100

    changed start page

--- roles/qutebrowser/files/config.py
+++ roles/qutebrowser/files/config.py
@@ -2,6 +2,8 @@ c.window.hide_wayland_decoration = False
 
 c.url.default_page = "https://www.qwant.com/"
 
+c.url.start_pages = ["https://www.qwant.com/"]
+
 c.url.searchengines = {
     "DEFAULT": "https://www.qwant.com/?q={}",
     "g": "https://www.google.com/search?q={}",

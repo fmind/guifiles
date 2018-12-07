ea2a8639849168daaa5b88682c79320bc0e34ff9
Médéric Hurier <dev@fmind.me>
2018-12-07 18:48:46 +0100

    added config

---
+++ roles/qutebrowser/files/config.py
@@ -0,0 +1,8 @@
+c.window.hide_wayland_decoration = False
+
+c.url.default_page = "https://www.qwant.com/"
+
+c.url.searchengines = {
+    "DEFAULT": "https://www.qwant.com/?q={}",
+    "g": "https://www.google.com/search?q={}",
+}

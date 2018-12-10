c2861a6a08fa6b0544ba72f73f98756067bd7f33
Médéric Hurier <dev@fmind.me>
2018-12-10 13:20:27 +0100

    updated config

--- roles/qutebrowser/files/config.py
+++ roles/qutebrowser/files/config.py
@@ -1,10 +1,11 @@
-c.window.hide_wayland_decoration = False
-
 c.url.default_page = "https://www.qwant.com/"
 
 c.url.start_pages = ["https://www.qwant.com/"]
 
 c.url.searchengines = {
     "DEFAULT": "https://www.qwant.com/?q={}",
+    "s": "https://stackoverflow.com/search?q={}",
     "g": "https://www.google.com/search?q={}",
+    "h": "https://github.com/search?q={}",
+    "q": "https://www.qwant.com/?q={}",
 }
